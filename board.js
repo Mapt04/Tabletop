af3ff6a1e83426bc26d3c200ec17b10badc2c1af
Alex Apt <alexaptib@gmail.com>
2020-11-06 15:51:19 -0300

    created Board class

--- board.js
+++ board.js
@@ -1,45 +1,58 @@
-function createBoard()
-{
-    const board = document.querySelector(".board");
-    let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
-    let index = 0;
-    let dark = false;
-    let num = 1;
-
-    for(let i=0; i < 8; i++)
-    {
-        $(".letters").append($("<li></li>")
-        .text(letters[i])
-        );
-
-        $(".numbers").append($("<li></li>")
-            .text(num++)
-            );
+class Board {
+    constructor() {
+        this.array = [];
+        for (let i = 0;i<8;i++){
+            let row = [];
+            for (let j = 0;j<8;j++){
+                row.push("");
+            }
+            this.array.push(row);
+        }
     }
 
-    for(let i=1; i<=64; i++)
+    createBoard()
     {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        if(dark){
-            square.classList.add("dark");
-        }
-        else{
-            square.classList.add("light");
-        }
-        index++;
-        dark=!dark;
-        
-        square.id = (8-Math.ceil(i/8))*8 + (64-i)%8 + (2 * (3.5-((64-i)%8))) + 1;
-
-        board.appendChild(square);
-
-        if(index % 8 == 0)
+        const board = document.querySelector(".board");
+        let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+        let index = 0;
+        let dark = false;
+        let num = 1;
+    
+        for(let i=0; i < 8; i++)
         {
-            dark = !dark;
-            index = 0;
+            $(".letters").append($("<li></li>")
+            .text(letters[i])
+            );
+    
+            $(".numbers").append($("<li></li>")
+                .text(num++)
+                );
         }
-
+    
+        for(let i=1; i<=64; i++)
+        {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            if(dark){
+                square.classList.add("dark");
+            }
+            else{
+                square.classList.add("light");
+            }
+            index++;
+            dark=!dark;
+            
+            square.id = (8-Math.ceil(i/8))*8 + (64-i)%8 + (2 * (3.5-((64-i)%8))) + 1;
+    
+            board.appendChild(square);
+    
+            if(index % 8 == 0)
+            {
+                dark = !dark;
+                index = 0;
+            }
+    
+        }
     }
-}
 
+  }
